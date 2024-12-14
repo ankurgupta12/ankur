4375cd5a2d5b68f90709277b4ad8b65a38ec8d65
ankur <agupta@parkoursc.com>
2024-12-14 16:10:17 +0530

    add the api url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    apiUrl:'https://226.244.72.148.host.secureserver.net:8080/no-deposite/',
+    apiUrl:'https://nodeposit.app:8080/no-deposite/',
     production:true,
 };
